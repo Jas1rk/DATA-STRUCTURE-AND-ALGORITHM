c73aba9f3ed6d0a146da73de69175425ac790b55
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2024-04-27 10:22:19 +0530

    initial commit

---
+++ week1/sample.js
@@ -0,0 +1,18 @@
+//  String reversing
+
+function reverseString(str){
+    return str.split('').reverse().join('')
+}
+console.log(reverseString('hello'))
+
+////////
+
+
+function sum(a,b){
+    return a+b
+}
+console.log(sum(10,20))
+
+
+
+//////
