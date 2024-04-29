88d8ce9ef41e723c4f6f6046a194f97cd8be042c
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2024-04-29 17:33:22 +0530

    sets

--- week1/sample.js
+++ week1/sample.js
@@ -26,3 +26,13 @@ object.phone = 12345678
 delete object.phone
 console.log(object)
 
+////Sets////
+
+const set = new Set([1, 2, 3, 4]);
+set.add(5)   //to add a new element
+console.log(set.has(3))   // the has method we use to check if the element is exist
+set.delete(3)   /// to delete an element
+set.clear()   //to clear all elements
+for (const item of set) {
+    console.log(item);
+}
