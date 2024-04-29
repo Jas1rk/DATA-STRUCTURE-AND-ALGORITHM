081f7385482c7840faf8ff016882d9d4a81c76f1
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2024-04-29 12:56:02 +0530

    array sample

---
+++ week1/array.js
@@ -0,0 +1,9 @@
+const array = [1,2,3,4]
+array.push(8) //to add new element in the last element of array
+array.unshift(0) // to add a new element in zero index of array 
+array.pop() // to remove an element in last of array
+array.shift() // to remove an element of first of array 
+for(const items of array){
+
+    console.log(items)
+}
