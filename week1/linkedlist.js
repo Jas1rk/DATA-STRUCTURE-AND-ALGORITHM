3b6f7f8f236a4c84f5059c6f54421f774868280e
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2024-05-02 19:07:37 +0530

    print method

--- week1/linkedlist.js
+++ week1/linkedlist.js
@@ -31,11 +31,30 @@ class LinkedList {
         this.size++
         
     }
+
+    print(){
+        if(this.isEmpty()){
+            console.log('Linked list is empty')
+        }else{
+            let current = this.head
+            let listOfValue = ' '
+            while(current){
+                listOfValue += `${current.value}`
+                current = current.next
+            }
+            console.log(listOfValue)
+        }
+    }
 }  
 
 const list  = new LinkedList()
 console.log('this is empty',list.isEmpty())
 console.log('this is size',list.getSize())
+list.print()
+
 list.prepend(100)
+list.print()
+
 list.prepend(300)
 list.prepend(400)
+list.print( )
