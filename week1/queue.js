393280d0417599b5e5218b30cfb70df8a94ca2ea
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2024-05-02 09:59:38 +0530

    queue optimized

--- week1/queue.js
+++ week1/queue.js
@@ -53,3 +53,51 @@ console.log('Removing the element in queue==>',queue.dequeue())
 console.log('after removing an element in queue==>>')
 queue.print()
 
+
+
+class Queue{
+  constructor(){
+    this.items = {}
+    this.front = 0
+    this.rear = 0
+  }
+
+  enqueue(element){
+    this.items[this.rear] = element
+    this.rear++
+  }
+
+  dequeue(){
+    const item = this.items[this.front]
+    delete this.items[this.front]
+    this.front++
+    return item
+  }
+
+  isEmpty(){
+    return this.front - this.rear === 0
+  }
+
+  peek(){
+    return this.items[this.front]
+  }
+
+  size(){
+    return this.front - this.rear
+  }
+
+  print(){
+     console.log(this.items)
+  }
+}
+
+const queue = new Queue()
+console.log(queue.isEmpty())
+queue.enqueue(100)
+queue.enqueue(400)
+queue.enqueue(700)
+queue.print()
+console.log(queue.peek())
+console.log(queue.dequeue())
+queue.print()
+
