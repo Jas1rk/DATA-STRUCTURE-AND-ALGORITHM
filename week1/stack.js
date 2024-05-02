ae16eec5a42d665686e45b5505a62f7cb3a23ec2
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2024-05-02 10:32:32 +0530

    minor changes

--- week1/stack.js
+++ week1/stack.js
@@ -1,51 +1,56 @@
-class Stack {
-    constructor(){
-        this.items = [];
-    }
+// class Stack {
+//     constructor(){
+//         this.items = [];
+//     }
    
-    push(element){  // Adding an element to the stack
-        this.items.push(element);
-    }
+//     push(element){  // Adding an element to the stack
+//         this.items.push(element);
+//     }
     
-    pop(){ // Remove the top element from the stack 
-        if(!this.isEmpty()){
-            return this.items.pop();
-        }
-        return 'underflow';
-    }
+//     pop(){ // Remove the top element from the stack 
+//         if(!this.isEmpty()){
+//             return this.items.pop();
+//         }
+//         return 'underflow';
+//     }
 
-    peek(){  // Returns the top element of the stack without removing it
-        if(!this.isEmpty()){
-            return this.items[this.items.length - 1];
-        }
-    }
+//     peek(){  // Returns the top element of the stack without removing it
+//         if(!this.isEmpty()){
+//             return this.items[this.items.length - 1];
+//         }
+//     }
 
-    isEmpty(){ // Check if the stack is empty
-        return this.items.length === 0;
-    }
+//     isEmpty(){ // Check if the stack is empty
+//         return this.items.length === 0;
+//     }
 
-    size(){
-        return this.items.length;
-    }
+//     size(){
+//         return this.items.length;
+//     }
 
-    print(){
-        let str = '';
-        for(let i = 0; i < this.items.length; i++){
-            str += this.items[i] + ' ';
-        }
-        return str;
-    }
+//     print(){
+//         let str = '';
+//         for(let i = 0; i < this.items.length; i++){
+//             str += this.items[i] + ' ';
+//         }
+//         return str;
+//     }
 
-}
+// }
 
-const stack = new Stack();
-stack.push(900);
-stack.push(897);
-stack.push(675);
-console.log('These are the elements in the stack:');
-console.log(stack.print());
-console.log('checking the element in front')
-console.log(stack.peek())
-console.log('remove the top element in stack',stack.pop())
+// const stack = new Stack();
+// stack.push(900);
+// stack.push(897);
+// stack.push(675);
+// console.log('These are the elements in the stack:');
+// console.log(stack.print());
+// console.log('checking the element in front')
+// console.log(stack.peek())
+// console.log('remove the top element in stack',stack.pop())
 
-console.log(stack.print())
+// console.log(stack.print())
+
+
+
+
+
