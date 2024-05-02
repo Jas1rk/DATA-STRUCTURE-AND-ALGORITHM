2ca0d80b2128cf834ffcad9f0c9aa13575019fe4
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2024-05-02 14:33:10 +0530

    Queue optimized

--- week1/queue.js
+++ week1/queue.js
@@ -91,13 +91,100 @@ class Queue{
   }
 }
 
-const queue = new Queue()
+const queue1 = new Queue()
 console.log(queue.isEmpty())
-queue.enqueue(100)
-queue.enqueue(400)
-queue.enqueue(700)
-queue.print()
-console.log(queue.peek())
-console.log(queue.dequeue())
-queue.print()
+queue1.enqueue(100)
+queue1.enqueue(400)
+queue1.enqueue(700)
+queue1.print()
+console.log(queue1.peek())
+console.log(queue1.dequeue())
+queue1.print()
 
+
+
+
+///Cirularqueue///
+
+  class CircularQueue {
+  constructor(capacity) {
+    this.items = new Array(capacity)
+    this.capacity = capacity
+    this.currentLength = 0
+    this.front = -1
+    this.rear = -1
+  }
+
+  isFull(){ //this line represents the queue is full
+    return this.currentLength === this.capacity
+  }
+
+  isEmpty(){
+    return  this.currentLength === 0
+  }
+
+  enqueue(element){
+    if(!this.isFull()){
+      this.rear =( this.rear + 1) %  this.capacity
+      this.items[this.rear] = element
+      this.currentLength += 1
+      if(this.front === -1){
+        this.front = this.rear
+      }
+    }
+  }
+
+  dequeue(){
+    if(this.isEmpty()){
+      return null
+    }
+    const item = this.items[this.front]
+    this.items[this.front] = null
+    this.front = (this.front +1 ) % this.capacity
+    this.currentLength -=1
+    if(this.isEmpty){
+      this.front -=1
+      this.rear -=1
+    }
+    return item
+  }
+
+  peek(){
+    if(!this.isEmpty()){
+      return this.items[this.front]
+    }
+    return null
+
+  }
+
+  print(){
+    if(this.isEmpty()){
+      console.log('The queue is Empty ')
+    }else{
+      let i
+      let str = ""
+      for( i = this.front; i !== this.rear; i=(i+1)%this.capacity){
+        str += this.items[i] + " "
+      }
+      str = this.items[i]
+      console.log(str)
+
+    }
+  }
+
+}
+
+const queue2 = new CircularQueue(5)
+
+queue2.enqueue(100)
+queue2.enqueue(200)
+queue2.enqueue(300)
+queue2.enqueue(400)
+queue2.enqueue(500)
+
+
+console.log(queue2.isEmpty())
+console.log(queue2.isFull())
+queue2.print()
+console.log(queue2.dequeue())
+queue2.print()
