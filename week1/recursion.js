25fa032cae30a74bdc9adffb6fcf73f70ac96aff
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2024-05-14 13:06:28 +0530

    recursive fibonacci

---
+++ week1/recursion.js
@@ -0,0 +1,9 @@
+function recursiveFibonacci(n){
+    if(n<2){
+        return n
+    }
+   return recursiveFibonacci(n-1)+recursiveFibonacci(n-2)
+}
+console.log(recursiveFibonacci(6))
+console.log(recursiveFibonacci(0))
+console.log(recursiveFibonacci(1))
